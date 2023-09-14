99a53e287ff4f2eb71a6db13dab751d42b074dd5
Maxim Gres <chemax715511@gmail.com>
2023-09-14 16:06:38 +0300

    adaptive added

--- src/components/nav-menu/nav-menu.jsx
+++ src/components/nav-menu/nav-menu.jsx
@@ -3,15 +3,34 @@ import Search from "./search"
 import { Link } from "react-router-dom"
 
 function NavMenu() {
+
+    const toggle = () => {
+        const menu = document.querySelector('.nav-menu__items')
+        const menuBtn = document.querySelector('.menu-btn')
+        menu.classList.toggle('active')
+        menuBtn.classList.toggle('active')
+    }
+
     return (
         <nav className="nav-menu">
-            <Link to={`/`}>
-                <div className="nav-menu__logo">Vilm</div>
-            </Link>
-            <Link to={`/my-tickets`}>
-                <div className="nav-menu__my-tickets">My Tickets</div>
-            </Link>
-            <Search></Search>
+            <div className="nav-menu-logo-btn">
+                <Link to={`/`}>
+                    <div className="nav-menu__logo">Vilm</div>
+                </Link>
+                <div className="btn-container">
+                    <div className="menu-btn" onClick={toggle}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+            <div className="nav-menu__items">
+                <Link to={`/my-tickets`}>
+                    <div className="nav-menu__my-tickets">My Tickets</div>
+                </Link>
+                <Search></Search>
+            </div>
         </nav>
     )
 }
